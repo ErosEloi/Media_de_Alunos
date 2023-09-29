05613635d176070aacc62f362259693246339d1b
Eros Eloi <eroseloi873@gmail.com>
2023-09-29 00:07:00 -0300

    Criação da classe estudante

---
+++ src/Application/Estudante.java
@@ -0,0 +1,13 @@
+package Application;
+
+public class Estudante {
+
+	public String Nome;
+	public double Nota1, Nota2, Nota3;
+	public double Total;
+
+	public double Calcular_total(double n1, double n2, double n3) {
+		return Total = (Nota1 + Nota2 + Nota3);
+	}
+
+}
--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,9 +1,18 @@
+/* 
+ >Exercício 2<
+ Fazer um programa para ler  nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+ (Primeiro trimestre vale 30 e o segundo e terceiro vale 35 cada). Ao final, mostrar a nota final do aluno no ano.
+ Dizer se o aluno está aprovado ou não e, em caso de negativo, quantos pontos faltam para o aluno obter o minimo para ser aprovado
+ (60% da nota).
+ */
+
 package Application;
+
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
